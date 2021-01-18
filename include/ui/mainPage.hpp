2a0e048090995d3de48106c5d7180fc952cdff32
Christiaan Bloemendaal <christiaan@chimp.works>
2021-01-18 19:19:42 +0100

    Added menu item with dummy callback

--- include/ui/mainPage.hpp
+++ include/ui/mainPage.hpp
@@ -14,6 +14,7 @@ namespace inst::ui {
             void sigPatchesMenuItem_Click();
             void settingsMenuItem_Click();
             void exitMenuItem_Click();
+            void tinfoilMenuItem_Click();
             void onInput(u64 Down, u64 Up, u64 Held, pu::ui::Touch Pos);
             Image::Ref awooImage;
             Image::Ref titleImage;
@@ -26,6 +27,7 @@ namespace inst::ui {
             Rectangle::Ref botRect;
             pu::ui::elm::Menu::Ref optionMenu;
             pu::ui::elm::MenuItem::Ref installMenuItem;
+            pu::ui::elm::MenuItem::Ref tinfoilMenuItem;
             pu::ui::elm::MenuItem::Ref netInstallMenuItem;
             pu::ui::elm::MenuItem::Ref usbInstallMenuItem;
             pu::ui::elm::MenuItem::Ref sigPatchesMenuItem;
